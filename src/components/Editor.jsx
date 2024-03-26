7e9f170ebb8a45dcbd883c0fbaf66be93b6fcf39
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-03-26 19:29:37 +0530

    Codemirror Integrated Successfully

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+
+import CodeMirror from "codemirror";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/dracula.css";
+import "codemirror/mode/javascript/javascript.js";
+import "codemirror/addon/edit/matchbrackets";
 
 const Editor = () => {
+  const editorRef = useRef(null);
+  useEffect(() => {
+    async function init() {
+      if (editorRef.current) {
+        console.log("codemiror working");
+        CodeMirror.fromTextArea(editorRef.current, {
+          mode: {
+            name: "javascript",
+            json: true,
+          },
+          theme: "dracula",
+          lineNumbers: true,
+          autofocus: true,
+          autoCloseTags: true,
+          autoCloseBrackets: true,
+          matchBrackets: true,
+        });
+      }
+    }
+    init();
+  }, []);
   return (
     <div className="w-full h-full">
-      <textarea name="editor" id="editor" className="w-full h-full"></textarea>
+      <textarea
+        name="editor"
+        id="editor"
+        className="w-full h-full"
+        ref={editorRef}
+      ></textarea>
     </div>
   );
 };
