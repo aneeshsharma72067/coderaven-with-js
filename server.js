58cf1e95a2fef357f89cd3ae0af7eb738193855b
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-03-26 22:34:30 +0530

    Handling socket events

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import { ACTIONS } from "./actions.js";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
 const PORT = process.env.PORT || 3000;
+
+const userSocketMap = {};
+const getAllConnectedClients = (roomId) => {
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return { socketId, userName: userSocketMap[socketId] };
+    }
+  );
+};
 
 io.on("connection", (socket) => {
   console.log("socket connected ", socket.id);
+  socket.on(ACTIONS.JOIN, ({ roomId, userName }) => {
+    userSocketMap[socket.id] = userName;
+    socket.join(roomId);
+    console.log(`${userName} joined the room ${roomId}`);
+
+    const clients = getAllConnectedClients(roomId);
+  });
+  socket.on(ACTIONS.DISCONNECTED, () => {
+    console.log("User Disconnected");
+  });
 });
 
 server.listen(PORT, () => {
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,12 +1,45 @@
-import { useParams } from "react-router-dom";
+import {
+  Navigate,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
 import React, { useEffect, useRef } from "react";
 import AsideBar from "../components/AsideBar";
 import Editor from "../components/Editor";
+import { initSocket } from "../socket";
+import { ACTIONS } from "../../actions";
+import toast from "react-hot-toast";
 
 const Room = () => {
   const { roomId } = useParams();
   const socketRef = useRef(null);
-  useEffect(() => {}, []);
+  const location = useLocation();
+  const reactNavigate = useNavigate();
+
+  useEffect(() => {
+    async function init() {
+      socketRef.current = await initSocket();
+      socketRef.current("connect_error", (err) => handleErrors(err));
+      socketRef.current("connect_failed", (err) => handleErrors(err));
+
+      function handleErrors(err) {
+        console.log("Socket error", err);
+        toast.error("Socket Connection failed, try again later !");
+        reactNavigate("/");
+      }
+
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.userName,
+      });
+    }
+    init();
+  }, []);
+
+  if (!location.state) {
+    return <Navigate to={"/"} />;
+  }
 
   return (
     <div className="w-full flex text-white h-screen">
