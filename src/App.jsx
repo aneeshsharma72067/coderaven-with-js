4f7026c284e7131d543c49375a5db5a3265e8067
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-03-26 19:16:21 +0530

    Initialized React Router Dom with 2 new Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import React from "react";
+import Home from "./pages/Home";
+import Room from "./pages/Room.";
 
 const App = () => {
   return (
     <>
-      <div>Hello</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/editor/:roomId" element={<Room />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
